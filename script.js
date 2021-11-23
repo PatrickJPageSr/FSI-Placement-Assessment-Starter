db2bbb08e2663cc4db5f3df66b07ddd3056aab44
Patrick Page <pagefamily-imac@Patricks-iMac.local>
2021-11-22 21:26:05 -0800

    assessment

---
+++ script.js
@@ -0,0 +1,49 @@
+
+let yourName = "Patrick Page";
+
+document.getElementById('credit').textContent = `Created by ${yourName}`;
+
+// selectors for each indv cookie div
+let gbDiv = document.querySelector("#Gingerbread");
+let ccDiv = document.querySelector("#Chocolate Chip");
+let ssDiv = document.querySelector("#Sugar Sprinkle");
+
+//set count tracker
+
+// Gingerbread
+let gbCount = 0;      
+// Chocolate Chip
+let ccCount = 0;      
+// Sugar Sprinkle
+let sugarCount = 0;   
+
+
+// created and append gb add button
+let GingebreadBtn = document.createElement('button');
+
+    counterPlusElem.addEventListener("click",()=>{
+    counter++;
+});
+    counterPlusElem.addEventListener("click",()=>{
+    counter--;
+});
+updateDisplay(){
+    counterDisplayElem.innerHTML = count;
+};
+
+gbDiv.append(PlusElem);
+counterPlusElem.addEventListener("click",()=>{
+
+});
+
+// created and append gb minus button
+document.getElementById('minus-gb').addEventListener('click', function() {
+    id="add-gb";
+    });
+
+    gbDiv.append
+    counterMinusElem.addEventListener("click", function()=>{
+    
+    });
+
+
